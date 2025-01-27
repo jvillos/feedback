92d1bdaa98e25d5c6938748dac469b9fc0acf214
jvillos <jvillosl14@gmail.com>
2025-01-27 10:34:31 +0100

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import FeedbackList from "./FeedbackList/FeedbackList";
+import SelectedFeedbackDisplay from "./SelectedFeedbackDisplay/SelectedFeedbackDisplay";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <h1>Feedback emoji app</h1>
+      <FeedbackList />
+      <SelectedFeedbackDisplay />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/SelectedFeedbackDisplay/SelectedFeedbackDisplay.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SelectedFeedbackDisplay() {
+  return <div>SelectedFeedbackDisplay</div>;
+}
+
+export default SelectedFeedbackDisplay;
